097551a1e9f47947085f715c0748e4e9934a55fe
Jérôme <91902262+JeromeCornu@users.noreply.github.com>
2021-12-08 17:06:22 +0100

    Character Can PickUp

---
+++ Assets/Scripts/Jerome/Pickup.cs
@@ -0,0 +1,27 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Pickup : MonoBehaviour
+{
+    public Transform TheHand;
+
+    //Pick
+    void OnMouseDown()
+    {
+        GetComponent<SphereCollider>().enabled = false;
+        GetComponent<Rigidbody>().useGravity = false;
+        GetComponent<Rigidbody>().isKinematic = true;
+        this.transform.position = TheHand.position;
+        this.transform.parent = GameObject.Find("Hand").transform;
+    }
+
+    //Throw
+    void OnMouseUp()
+    {
+        this.transform.parent = null;
+        GetComponent<Rigidbody>().useGravity = true;
+        GetComponent<Rigidbody>().isKinematic = false;
+        GetComponent<SphereCollider>().enabled = true;
+    }
+}
