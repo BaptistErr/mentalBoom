216c1339db07edf6138adc362d12017793072f20
Jérôme <91902262+JeromeCornu@users.noreply.github.com>
2021-12-09 17:54:18 +0100

    LimitedDistance pickUp

--- Assets/Scripts/Jerome/Pickup.cs
+++ Assets/Scripts/Jerome/Pickup.cs
@@ -5,15 +5,22 @@ using UnityEngine;
 public class Pickup : MonoBehaviour
 {
     public Transform TheHand;
+    private GameObject Character;
+    public float MaxDistance = 1f;
 
     //Pick
     void OnMouseDown()
     {
-        GetComponent<SphereCollider>().enabled = false;
-        GetComponent<Rigidbody>().useGravity = false;
-        GetComponent<Rigidbody>().isKinematic = true;
-        this.transform.position = TheHand.position;
-        this.transform.parent = GameObject.Find("Hand").transform;
+        Character = GameObject.Find("Character");
+        float distance = Vector3.Distance(this.transform.position, Character.transform.position);
+        if(distance < MaxDistance)
+        {
+            GetComponent<SphereCollider>().enabled = false;
+            GetComponent<Rigidbody>().useGravity = false;
+            GetComponent<Rigidbody>().isKinematic = true;
+            this.transform.position = TheHand.position;
+            this.transform.parent = GameObject.Find("Hand").transform;
+        }
     }
 
     //Throw
