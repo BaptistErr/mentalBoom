5f5c7a0362fcb905265ea7f69409dc4f6efb12ca
Arekva <tutur716@gmail.com>
2021-12-10 19:14:52 +0100

    Dash, ""merge"" of the duplicated script

--- Assets/Scripts/CharacterController.cs
+++ Assets/Scripts/CharacterController.cs
@@ -8,9 +8,10 @@ using UnityEngine;
 public class CharacterController : MonoBehaviour
 {
     
-#region Movement
+#region Movements
 
     [SerializeField] private float _moveSpeed = 5.0F;
+    /// <summary> The normal walk speed, without any other effect. </summary>
     public float MoveSpeed
     {
         get => _moveSpeed;
@@ -18,12 +19,17 @@ public class CharacterController : MonoBehaviour
     }
     
     [SerializeField] private float _dashMoveSpeed = 15.0F;
+    /// <summary> The walk speed when dashing.
+    /// The player cannot change its direction when a dash is performed. </summary>
     public float DashMoveSpeed
     {
         get => _dashMoveSpeed;
         set => _dashMoveSpeed = value;
     }
+    
     [SerializeField] private float _dashDuration = 0.5F;
+    /// <summary> The duration of a dash.
+    /// When the character hits an obstacle, the dash immediately stops. </summary>
     public float DashDuration
     {
         get => _dashDuration;
@@ -31,10 +37,28 @@ public class CharacterController : MonoBehaviour
     }
     
     [SerializeField] private float _dashCooldown = 0.5F;
+    /// <summary> How long does the player has to wait after a dash is finished. </summary>
     public float DashCooldown
     {
         get => _dashCooldown;
         set => _dashCooldown = value;
+    }
+    
+    [SerializeField] private bool _isJumpEnabled = true;
+    /// <summary> Is the jump globally enabled or not. </summary>
+    public bool IsJumpEnabled
+    {
+        get => _isJumpEnabled;
+        set => _isJumpEnabled = value;
+    }
+    
+    
+    [SerializeField] private float _jumpForce = 50.0F;
+    /// <summary> The force applied when jumping (in newton). </summary>
+    public float JumpForce
+    {
+        get => _jumpForce;
+        set => _jumpForce = value;
     }
     
 #endregion
@@ -47,33 +71,49 @@ public class CharacterController : MonoBehaviour
     // forward to the camera.
     
     // X Axis
+    /// <summary> The left direction relative to the character's controls.
+    /// Is is computed from the cross product of Forward and Up. </summary>
     public Vector3 LeftDirection => Vector3.Cross(ForwardDirection, UpDirection); // -X
+    /// <summary> The right direction relative to the character's controls. </summary>
     public Vector3 RightDirection => -LeftDirection; // +X
         
     // Y Axis
+    /// <summary> The up direction relative to the character's controls.
+    /// This is a constant. </summary>
     public Vector3 UpDirection => Vector3.up; // +Y
+    /// <summary> The down direction relative to the character's controls.
+    /// This is a constant. </summary>
     public Vector3 DownDirection => -UpDirection; // -Y
     
     // Z Axis
     [SerializeField] private Vector3 _forwardDirection = (Vector3.left + Vector3.forward).normalized; // +Z (back field)
+    /// <summary> The forward direction relative to the character's controls.
+    /// this is the only setting to set, then all the other directions of the X and Z axis will adapt. </summary>
     public Vector3 ForwardDirection // +Z (API)
     {
         get => _forwardDirection;
         set => _forwardDirection = value;
     }
+    /// <summary> The backward direction relative to the character's controls. </summary>
     public Vector3 BackwardDirection => -ForwardDirection; // -Z
     
 #endregion
 
-    private Rigidbody _rb; // rigidbody cache, no GetComponent<T>() spam, ugh.
-    private Collider _collider; // idem.
+    // component cache
+    private Rigidbody _rb;
+    private Collider _collider;
+    
+    // other
     private float _maxColliderExtent;
+    private bool _characterOnFloor;
 
     // dash state
     private bool _dashing;
     private Vector3 _dashDir;
     private float _dashTime;
     private float _timeSinceDashEnd = float.PositiveInfinity;
+
+#region Unity callbacks
 
     private void Awake()
     {
@@ -84,7 +124,7 @@ public class CharacterController : MonoBehaviour
         Vector3 extents = _collider.bounds.extents;
         _maxColliderExtent = Math.Max(extents.x, extents.z);
     }
-
+    
     private void Update()
     {
         if (!_dashing)
@@ -106,15 +146,12 @@ public class CharacterController : MonoBehaviour
             }
         }
     }
-
+    
     private void FixedUpdate()
     {
         if (_dashing)
         {
-            Ray wallTestRay = new Ray(origin: _rb.position, direction: _dashDir);
-            float checkDistance = Time.fixedDeltaTime*_dashMoveSpeed + _maxColliderExtent;
-
-            if (Physics.Raycast(ray: wallTestRay, maxDistance: checkDistance, layerMask: ~_collider.gameObject.layer))
+            if (IsObstacleOnFrame(direction: _dashDir, speed: _dashMoveSpeed))
             { // if any object has been hit, stop dashing
                 StopDash();
             }
@@ -125,15 +162,28 @@ public class CharacterController : MonoBehaviour
         }
         else
         {
+            if (_isJumpEnabled && Input.GetButtonDown("Jump"))
+            {
+                Jump(_jumpForce);
+            }
+            
             Move(direction: GetMoveDirection(), speed: _moveSpeed);
         }
     }
+    
+    private void OnCollisionEnter(Collision collision)
+    {
+        if (collision.gameObject.CompareTag("Jumpable"))
+        {
+            _characterOnFloor = true;
+        }
+    }
+    
+#endregion
 
     private void Move(Vector3 direction, float speed)
     {
-        Vector3 dir = GetMoveDirection();
         Vector3 translation = direction * speed * Time.fixedDeltaTime;
-        
         _rb.MovePosition(_rb.position + translation);
     }
     
@@ -142,7 +192,7 @@ public class CharacterController : MonoBehaviour
         Vector3 dir = GetMoveDirection();
         if (dir != Vector3.zero)
         {
-            _dashDir = GetMoveDirection();
+            _dashDir = dir;
             _dashing = true;
             _dashTime = 0.0F;
         }
@@ -153,11 +203,30 @@ public class CharacterController : MonoBehaviour
         _timeSinceDashEnd = 0.0F;
     }
 
+    private void Jump(float force)
+    {
+        if (_characterOnFloor)
+        {
+            _rb.AddForce(new Vector3(0, force, 0), ForceMode.Impulse);
+            _characterOnFloor = false;
+        }
+    }
+
     private Vector3 GetMoveDirection()
     {
         float horizontal = Input.GetAxis("Horizontal");
         float vertical = Input.GetAxis("Vertical");
 
         return (ForwardDirection*vertical + RightDirection*horizontal).normalized;
+    }
+    
+    private bool IsObstacleOnFrame(Vector3 direction, float speed)
+    {
+        if(_dashing) Debug.DrawRay(_rb.position, direction, Color.yellow, 1.0F);
+        return Physics.Raycast(
+            ray: new Ray(origin: _rb.position, direction),
+            maxDistance: Time.fixedDeltaTime * speed + _maxColliderExtent,
+            layerMask: ~_collider.gameObject.layer
+        );
     }
 }
