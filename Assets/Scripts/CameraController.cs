75af7e6d579d05b7c0289083e6f46162ed17d51c
Arekva <tutur716@gmail.com>
2021-12-13 11:10:44 +0100

    Fixed bébou's level

--- Assets/Scripts/CameraController.cs
+++ Assets/Scripts/CameraController.cs
@@ -2,13 +2,33 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
+
+public enum CameraMode
+{
+    FollowPlayer = 0,  // The camera statically follows the player. This is the default mode.
+    Weighed      = 1,  // The camera is placed in-between two objects where each of the objects weight can be adjusted.
+}
+
 public class CameraController : MonoBehaviour
 {
     public CharacterController Character;
     public Vector3 Offset;
+
+    private CameraMode _cameraMode = CameraMode.FollowPlayer;
+    public CameraMode CameraMode
+    {
+        get => _cameraMode;
+        set => _cameraMode = value;
+    }
     
+    
+
     private void LateUpdate()
     {
-        this.transform.position = Character.transform.position + Offset;
+        this.transform.position = CameraMode switch
+        {
+            CameraMode.FollowPlayer => Character.transform.position + Offset,
+            
+        };
     }
 }
